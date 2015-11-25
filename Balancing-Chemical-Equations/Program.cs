71735271bd6a04e6546d6bb6a74424d9661730a2
Zopherus <1012746@IHS60261436.ad.nsd.org>
2015-11-25 13:55:31 -0800

    Implemented RREF

--- Balancing-Chemical-Equations/Program.cs
+++ Balancing-Chemical-Equations/Program.cs
@@ -20,7 +20,97 @@ namespace Balancing_Chemical_Equations
             int middle = equation.IndexOf('-');
             string leftSide = equation.Substring(0, middle - 1);
             string rightSide = equation.Substring(middle + 3, equation.Length - middle - 3);
-            return leftSide + " -> " + rightSide;
+
+			List<string> leftSideTerms = SplitIntoTerms(leftSide);
+			List<string> rightSideTerms = SplitIntoTerms(rightSide);
+			
+            return rightSideTerms.ElementAt(0);
         }
+
+		static List<string> SplitIntoTerms(string equation)
+		{
+			List<string> terms = new List<string>();
+			string nextTerm = "";
+			for (int counter = 0; counter < equation.Length; counter++)
+			{
+				if (equation.ElementAt(counter) == '+')
+				{
+					terms.Add(nextTerm.Trim());
+					nextTerm= "";
+				}
+				else
+					nextTerm += equation.ElementAt(counter);
+			}
+			terms.Add(nextTerm.Trim());
+			return terms;
+		}
+
+		static double[,] ReducedRowEchelonForm(double[,] matrix)
+		{
+			int lead = 0, rowCount = matrix.GetLength(0), columnCount = matrix.GetLength(1);
+			for (int r = 0; r < rowCount; r++)
+			{
+				if (columnCount <= lead)
+					break;
+				int i = r;
+				while (matrix[i, lead] == 0)
+				{
+					i++;
+					if (i == rowCount)
+					{
+						i = r;
+						lead++;
+						if (columnCount == lead)
+						{
+							lead--;
+							break;
+						}
+					}
+				}
+				for (int j = 0; j < columnCount; j++)
+				{
+					double temp = matrix[r, j];
+					matrix[r, j] = matrix[i, j];
+					matrix[i, j] = temp;
+				}
+				double div = matrix[r, lead];
+				if (div != 0)
+				{
+					for (int j = 0; j < columnCount; j++) 
+					{
+						matrix[r, j] /= div;
+					}
+				}
+				for (int j = 0; j < rowCount; j++)
+				{
+					if (j != r)
+					{
+						double sub = matrix[j, lead];
+						for (int k = 0; k < columnCount; k++)
+						{
+							matrix[j, k] -= (sub * matrix[r, k]);
+						}
+					}
+				}
+				lead++;
+			}
+			return matrix;
+		}
+
+		static string ToString(double[,] grid)
+		{
+			string result = "";
+			for (int x = 0; x < grid.GetLength(0); x++)
+			{
+				for (int y = 0; y < grid.GetLength(1); y++)
+				{
+					result += grid[x, y].ToString();
+					if (y != grid.GetLength(1) - 1)
+						result += ",";
+				}
+				result += "\n";
+			}
+			return result;
+		}
     }
 }
